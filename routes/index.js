410b6f0687f7675f9d03f5902db477407ba6461d
Jason Kerr <jkerr013@gmail.com>
2019-05-28 19:11:24 -0400

    trying to get stripe to work with form

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 var Product = require('../models/product')
+var Cart = require('../models/cart')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,6 +12,39 @@ router.get('/', function (req, res, next) {
       return
     }
     res.render('shop/index', { title: 'Shopping Cart', products: docs })
+  })
+})
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+  var productId = req.params.id
+  var cart = new Cart(req.session.cart ? req.session.cart : { items: {} })
+
+  Product.findById(productId, function (err, product) {
+    if (err) {
+      console.log(err)
+      return res.redirect('/')
+    }
+    cart.add(product, product.id)
+    req.session.cart = cart
+    res.redirect('/')
+  })
+})
+
+router.get('/shopping-cart', function (req, res, next) {
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart', { products: null })
+  }
+  var cart = new Cart(req.session.cart)
+  res.render('shop/shopping-cart', { products: cart.generateArray(), totalPrice: cart.totalPrice })
+})
+
+router.get('/checkout', function (req, res, next) {
+  if (!req.session.cart) {
+    return res.render('shop/shopping-cart')
+  }
+  var cart = new Cart(req.session.cart)
+  res.render('shop/checkout', {
+    total: cart.totalPrice
   })
 })
 
